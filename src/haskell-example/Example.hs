a3ebd8c63a7c4501d7f0e0725f3cb69cad573a77
Jonathan Knowles <mail@jonathanknowles.net>
2022-05-21 08:03:19 +0800

    Add CI.

--- src/haskell-example/Example.hs
+++ src/haskell-example/Example.hs
@@ -2,3 +2,14 @@
 -- Example documentation.
 --
 module Example where
+
+import Example.Internal ()
+
+foo :: Int -> Int
+foo = id
+
+bar :: String -> String
+bar = id
+
+baz :: Double -> Double
+baz = id
